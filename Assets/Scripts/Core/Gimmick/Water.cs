95349862db9e3e826307331937eb5e00d72bfcdd
Kim Jeoung Gi <kjk9982098@daum.net>
2025-01-13 17:01:19 +0900

    맵 업데이트
    임시맵 업데이트

---
+++ Assets/Scripts/Core/Gimmick/Water.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger entered by: " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player detected in trigger.");
+            CharacterBase cb = collision.gameObject.GetComponent<CharacterBase>();
+            if (cb != null)
+            {
+                Debug.Log("PlayerCharacter component found, setting IsOnWater to true.");
+                cb.IsOnWater = true;
+            }
+        }
+    }
+}
